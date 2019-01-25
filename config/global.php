39254ccaa58646944cfe41f2b912010d7dcdc83f
Fatmoubidou <fatma.elammari@gmail.com>
2019-01-25 14:39:38 +0100

    global file

--- config/global.php
+++ config/global.php
@@ -3,7 +3,7 @@
 function getGlobalConfig() {
   return $config = [
     "protocol" => "",
-    "host" => "localhost/Lab/projetAdepObjet/",
+    "host" => "localhost/Lab/projet_adepV2/",
     "status" => ["anonymous", "user", "admin"],
     "defaultRoute" => ""
   ];
